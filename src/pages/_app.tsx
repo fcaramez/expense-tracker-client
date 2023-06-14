7dcb5a02be81732d0cf99fc27cf84f9b5cd2f7c8
fcaramez <francisco.caramez.duarte@gmail.com>
2023-06-14 01:41:23 +0100

    adds signup route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,12 +10,10 @@ const config: ThemeConfig = {
   useSystemColorMode: false,
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const theme = extendTheme({ config });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
---
+++ src/pages/api/signup.ts
@@ -0,0 +1,93 @@
+import { type NextApiRequest, type NextApiResponse } from 'next';
+import { prisma } from '~/server/db';
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+
+interface RequestBody extends NextApiRequest {
+  body: {
+    email: string;
+    password: string;
+    username: string;
+  };
+}
+
+interface ApiResponse {
+  message: string;
+  data?: Record<string, any>;
+  success: boolean;
+}
+
+const SECRET = process.env.TOKEN_SECRET as string;
+
+export async function index(
+  req: RequestBody,
+  res: NextApiResponse<ApiResponse>,
+) {
+  if (req.method !== 'POST') {
+    return res.status(400).json({
+      success: false,
+      message: 'This route does not exist',
+    });
+  }
+
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: 'Fields are mandatory',
+      });
+    }
+
+    const userToFind = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userToFind) {
+      return res.status(400).json({
+        success: false,
+        message: 'User already exists',
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const createdUser = await prisma.user.create({
+      data: {
+        email: email,
+        password: hashedPassword,
+        username: username,
+      },
+      select: {
+        userId: true,
+        username: true,
+        email: true,
+      },
+    });
+
+    const payload = {
+      email: createdUser.email,
+      username: createdUser.username,
+      userId: createdUser.userId,
+    };
+
+    const token = jwt.sign(payload, SECRET);
+
+    res.status(200).json({
+      success: true,
+      message: `Welcome aboard ${createdUser.username}`,
+      data: {
+        token,
+        ...payload,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+}
