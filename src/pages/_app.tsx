8541ebb4ffb7fc6cee6930846bff83d75977bdd8
fcaramez <francisco.caramez.duarte@gmail.com>
2023-06-14 01:21:20 +0100

    adds chakraUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import "~/styles/globals.css";
+import { type AppType } from 'next/dist/shared/lib/utils';
+import {
+  ChakraProvider,
+  type ThemeConfig,
+  extendTheme,
+} from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const theme = extendTheme({ config });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
